fb1ad089b4ef91ec509b80da2b505a1a382fcb78
sba23243 <sba23243@student.cct.ie>
2025-11-17 18:48:36 +0000

    Added the barchart for the uber data

--- uber.py
+++ uber.py
@@ -23,3 +23,7 @@ data_load_state = st.text("Loading Data......Done!")
 
 st.subheader("Raw Data")
 st.write(data)
+
+st.subheader("Number of Pickups per hour")
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24)) [0]
+st.bar_chart(hist_values)
