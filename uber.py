1f1da49387ca1eddef4a141d65ddf3e554077e2e
sba23243 <sba23243@student.cct.ie>
2025-11-17 18:43:02 +0000

    Update code for the date column

--- uber.py
+++ uber.py
@@ -10,7 +10,7 @@ st.title("Uber Pickups in NYC")
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
   'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-DATE_COLUMN = "date/time"
+DATE_COLUMN="date/time"
 
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
